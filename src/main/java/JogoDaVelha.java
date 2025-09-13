4452c4373716872f0c7b7a89c9d11d846670c9c3
Pedro Henrique Santiago Silva <63151672+phss-cmd@users.noreply.github.com>
2025-09-13 14:52:31 -0300

    metodo para imprimir tabuleiro

--- src/main/java/JogoDaVelha.java
+++ src/main/java/JogoDaVelha.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class JogoDaVelha {
+
+    public static void main(String[] args) {
+
+        String[][] tabuleiro={
+
+            {"","",""},
+            {"","",""},
+            {"","",""},
+
+        };
+
+        imprimirTabuleiro(tabuleiro);
+
+    }
+
+    private static void imprimirTabuleiro(String[][] tabuleiroMostar){
+
+        for (int linha = 0; linha <=2;linha++){
+
+            for (int coluna = 0; coluna <=2; coluna++){
+
+                System.out.print(tabuleiroMostar[linha][coluna]+" | ");
+
+            }
+
+            System.out.println( );
+
+        }
+
+    }
+
 }
