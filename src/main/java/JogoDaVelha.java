c70bf38a8e46e28012322e89cf09328515427eb0
Pedro Henrique Santiago Silva <63151672+phss-cmd@users.noreply.github.com>
2025-09-17 22:15:01 -0300

    Projeto finalizado Jogo da Velha

--- src/main/java/JogoDaVelha.java
+++ src/main/java/JogoDaVelha.java
@@ -4,17 +4,13 @@ import java.util.Scanner;
 public class JogoDaVelha {
 
     public static void main(String[] args) {
-
         String[][] tabuleiro={
-
-            {"","",""},
-            {"","",""},
-            {"","",""},
-
+                {"","",""},
+                {"","",""},
+                {"","",""},
         };
 
         Scanner sc = new Scanner(System.in);
-
         String jogador = "x";
 
         while (true){
@@ -35,26 +31,54 @@ public class JogoDaVelha {
             }
 
             if (!tabuleiro[linha -1][coluna -1].isBlank()){
-
                 System.out.println("Posição informada já preenchida, Escolha uma em branco!");
-                    continue;
+                continue;
+            }
+
+            tabuleiro[linha -1][coluna -1]=jogador;
+
+            if (jogoFinalizado(tabuleiro, jogador)){
+                imprimirTabuleiro(tabuleiro);
+                break;
+            }
+
+            jogador = jogador.equals("x") ? "o" : "x";
+        }
+    }
+
+    private static boolean jogoFinalizado(String[][] tabuleiro,String jogador) {
+        for (int linha = 0; linha <=2;linha++){
+            if (tabuleiro[linha][0]==jogador && tabuleiro[linha][1]==jogador && tabuleiro[linha][2]==jogador) {
+                System.out.println("Você ganhou o jogo");
+                return true;
+            }
+        }
+
+        for (int coluna = 0; coluna <=2;coluna++){
+            if (tabuleiro[0][coluna]==jogador && tabuleiro[1][coluna]==jogador && tabuleiro[2][coluna]==jogador) {
+                System.out.println("Você ganhou o jogo");
+                return true;
 
             }
+        }
+
+        if (tabuleiro[0][0]==jogador && tabuleiro[1][1]==jogador && tabuleiro[2][2]==jogador) {
+            System.out.println("Você ganhou o jogo");
+            return true;
 
         }
 
-
-
+        if (tabuleiro[2][0]==jogador && tabuleiro[1][1]==jogador && tabuleiro[0][2]==jogador) {
+            System.out.println("Você ganhou o jogo");
+            return true;
+        }
+        return false;
     }
 
     private static void imprimirTabuleiro(String[][] tabuleiroMostar){
-
         for (int linha = 0; linha <=2;linha++){
-
             for (int coluna = 0; coluna <=2; coluna++){
-
                 System.out.print(tabuleiroMostar[linha][coluna]+" | ");
-
             }
 
             System.out.println( );
